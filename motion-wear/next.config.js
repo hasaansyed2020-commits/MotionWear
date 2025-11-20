deed3beea6b4531e899688322d0b55983db8907b
Muhammad Hasaan Syed <hasaansyed@Muhammads-MacBook-Air.local>
2025-11-20 12:29:29 +0500

    this is first commit

--- motion-wear/next.config.js
+++ motion-wear/next.config.js
@@ -3,7 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'images.pexels.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
+    ],
   },
   transpilePackages: ['three'],
 }
